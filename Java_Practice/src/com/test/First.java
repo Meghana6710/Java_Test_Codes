a92085588edf701eddeee8f4307ad89a12ca842a
Meghana 1. Nagaraju (EXT-Nokia) <meghana.1.nagaraju.ext@nokia.com>
2024-02-15 17:31:32 +0530

    First Commit

---
+++ Java_Practice/src/com/test/First.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hey, this is my first program");
+
+	}
+
+}
---
+++ Java_Practice/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author menagara
+ *
+ */
+module Java_Practice {
+}
